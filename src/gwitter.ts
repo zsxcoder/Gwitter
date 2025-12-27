bf6cd322b3dd480cdad1b9ef44a2cc2e31c6c011
mcyzsx <mcyzsx@outlook.com>
2025-12-27 13:54:28 +0800

    5

--- src/gwitter.ts
+++ src/gwitter.ts
@@ -1,27 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AuthWindow from './AuthWindow';
 import { setConfig } from './config';
 import './i18n';
 import type { GwitterOptions } from './types/global';
-import { parseUrl } from './utils';
 
 let gwitterInstance: ReactDOM.Root | null = null;
 
 function renderGwitter(container: HTMLElement) {
-  const params = parseUrl();
-  let component = App;
-  if (params.code) {
-    component = AuthWindow;
-  }
-
   if (gwitterInstance) {
     gwitterInstance.unmount();
   }
 
   const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(component));
+  root.render(React.createElement(App));
 
   gwitterInstance = root;
 
